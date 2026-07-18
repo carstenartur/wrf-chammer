d25cfef65444dd3aa4c51bd02359cf7526629bb8
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-18 20:50:12 +0200

    Render system readiness before simulation workflow

--- workbench/ui/src/main.tsx
+++ workbench/ui/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { SystemReadiness } from './features/system/SystemReadiness';
 import './styles.css';
 
 const root = document.getElementById('root');
@@ -9,4 +10,11 @@ if (!root) {
   throw new Error('Missing root element');
 }
 
-createRoot(root).render(React.createElement(App));
+createRoot(root).render(
+  React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(SystemReadiness),
+    React.createElement(App),
+  ),
+);
