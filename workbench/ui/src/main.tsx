a84a0d3f3569ab903efff93a4d8f7b1de1b7c0ff
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-20 19:33:41 +0200

    Add persistent queue panel to Workbench UI

--- workbench/ui/src/main.tsx
+++ workbench/ui/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { Era5DataPanel } from './features/data/Era5DataPanel';
+import { JobQueuePanel } from './features/jobs/JobQueuePanel';
 import { SimulationWizard } from './features/simulation/SimulationWizard';
 import { SystemReadiness } from './features/system/SystemReadiness';
 import './styles.css';
@@ -19,6 +20,7 @@ createRoot(root).render(
     React.createElement(SystemReadiness),
     React.createElement(SimulationWizard),
     React.createElement(Era5DataPanel),
+    React.createElement(JobQueuePanel),
     React.createElement(App),
   ),
 );
